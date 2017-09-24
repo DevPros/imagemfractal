8d34ac50ad925cc4bb23b94d851933fbe2122c54
barcelosrui <barcelos.rui@gmail.com>
2017-09-25 00:58:59 +0100

    ADD THREAD to main

--- src/fractal/APPFractal.java
+++ src/fractal/APPFractal.java
@@ -1,6 +1,5 @@
 package fractal;
 
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -8,20 +7,23 @@ import javax.swing.JPanel;
  *
  * @author canoso
  */
-public class APPFractal extends JPanel{
-    
+public class APPFractal extends JPanel {
+
     //FractalImage img = new FractalImage();
-   
     public static void main(String[] args) {
-        JFrame window = new JFrame("Fractal");
-        FractalImage f = new FractalImage(800,600);// exit on close
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // center window
-        window.setVisible(true);
-        window.getContentPane().add(f);
-        window.pack();
-        window.setLocationRelativeTo(null);
-        
-     
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame window = new JFrame("Fractal");
+                FractalImage f = new FractalImage(800, 600);// exit on close
+                window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                // center window
+                window.setVisible(true);
+                window.getContentPane().add(f);
+                window.pack();
+                window.setLocationRelativeTo(null);
+            }
+
+        });
     }
 }
