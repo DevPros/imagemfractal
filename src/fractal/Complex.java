3f7c93f09dc97149897a457a0106021ba2290b89
João Canoso <jpcanoso@hotmail.com>
2017-10-23 17:43:06 +0100

    Removed unused complex functions

--- src/fractal/Complex.java
+++ src/fractal/Complex.java
@@ -4,7 +4,6 @@ package fractal;
 Copyright © 2000–2011, Robert Sedgewick and Kevin Wayne. 
 Last updated: Tue Aug 30 09:58:33 EDT 2016.
 */
-import java.util.Objects;
 
 public class Complex {
     private final double re;   // the real part
@@ -14,25 +13,6 @@ public class Complex {
     public Complex(double real, double imag) {
         re = real;
         im = imag;
-    }
-
-    // return a string representation of the invoking Complex object
-    @Override
-    public String toString() {
-        if (im == 0) return re + "";
-        if (re == 0) return im + "i";
-        if (im <  0) return re + " - " + (-im) + "i";
-        return re + " + " + im + "i";
-    }
-
-    // return abs/modulus/magnitude
-    public double abs() {
-        return Math.hypot(re, im);
-    }
-
-    // return angle/phase/argument, normalized to be between -pi and pi
-    public double phase() {
-        return Math.atan2(im, re);
     }
 
     // return a new Complex object whose value is (this + b)
@@ -59,75 +39,16 @@ public class Complex {
         return new Complex(real, imag);
     }
 
-    // return a new object whose value is (this * alpha)
-    public Complex scale(double alpha) {
-        return new Complex(alpha * re, alpha * im);
-    }
-
-    // return a new Complex object whose value is the conjugate of this
-    public Complex conjugate() {
-        return new Complex(re, -im);
-    }
-
-    // return a new Complex object whose value is the reciprocal of this
-    public Complex reciprocal() {
-        double scale = re*re + im*im;
-        return new Complex(re / scale, -im / scale);
-    }
-
     // return the real or imaginary part
     public double re() { return re; }
     public double im() { return im; }
-
-    // return a / b
-    public Complex divides(Complex b) {
-        Complex a = this;
-        return a.times(b.reciprocal());
-    }
-
-    // return a new Complex object whose value is the Complex exponential of this
-    public Complex exp() {
-        return new Complex(Math.exp(re) * Math.cos(im), Math.exp(re) * Math.sin(im));
-    }
-
-    // return a new Complex object whose value is the Complex sine of this
-    public Complex sin() {
-        return new Complex(Math.sin(re) * Math.cosh(im), Math.cos(re) * Math.sinh(im));
-    }
-
-    // return a new Complex object whose value is the Complex cosine of this
-    public Complex cos() {
-        return new Complex(Math.cos(re) * Math.cosh(im), -Math.sin(re) * Math.sinh(im));
-    }
-
-    // return a new Complex object whose value is the Complex tangent of this
-    public Complex tan() {
-        return sin().divides(cos());
-    }
-    
-
-
+  
     // a static version of plus
     public static Complex plus(Complex a, Complex b) {
         double real = a.re + b.re;
         double imag = a.im + b.im;
         Complex sum = new Complex(real, imag);
         return sum;
-    }
-
-    // See Section 3.3.
-    @Override
-    public boolean equals(Object x) {
-        if (x == null) return false;
-        if (this.getClass() != x.getClass()) return false;
-        Complex that = (Complex) x;
-        return (this.re == that.re) && (this.im == that.im);
-    }
-
-    // See Section 3.3.
-    @Override
-    public int hashCode() {
-        return Objects.hash(re, im);
     }
     
     public double distanceToOrigin() {
